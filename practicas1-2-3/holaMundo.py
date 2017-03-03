7e14439c1832ed8575a8cc24c7e2fbf4c91ee2a8
jadiazg02 <joseadiazg02@correo.ugr.es>
2017-03-03 17:37:19 +0100

    Creada estructura para virtualenv.

---
+++ practicas1-2-3/holaMundo.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+from flask import render_template, Response
+
+app = Flask(__name__)
+
+@app.route('/archivo')
+def ima():
+    response = Response()
+    response.headers['Content-Type']='image/jpg'
+
+    f= open('./static/ima/imagen.jpg', 'rb')
+
+    ima=f.read()
+    response.set_data(ima)
+    return response
+
+@app.route('/')
+def hello_world():
+
+    usuarios = []
+    usuarios.append({'name':'pepe','dni':2345})
+    usuarios.append({'name':'jose','dni':2323})
+    usuarios.append({'name':'pablo','dni':2356})
+
+    return render_template('hola.html', usuarios=usuarios)
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', debug='true')
