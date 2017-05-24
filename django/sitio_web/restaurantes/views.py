ef450bf6f4bf315606d687d1b228349e2106e47a
jadiazg02 <joseadiazg02@correo.ugr.es>
2017-05-24 13:18:58 +0200

    Añadidas urls y vistas básicas. avanza #10

--- django/sitio_web/restaurantes/views.py
+++ django/sitio_web/restaurantes/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('Hello World!')
+
+def test(request):
+    context = {}   # Aquí van la las variables para la plantilla
+    return render(request,'test.html', context)
