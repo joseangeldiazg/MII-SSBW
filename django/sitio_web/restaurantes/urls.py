57211b47923a5c14e3365fec25220603935827bc
jadiazg02 <joseadiazg02@correo.ugr.es>
2017-05-31 17:08:35 +0200

    Añadidas urls y vistas. Avanza #10.

--- django/sitio_web/restaurantes/urls.py
+++ django/sitio_web/restaurantes/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
   url(r'^$', views.index, name='index'),
   url(r'^test/$', views.test, name='test'),
+  url(r'^listar/$', views.listar, name='listar'),
+  url(r'^buscar/$', views.buscar, name='buscar'),
 ]
--- django/sitio_web/restaurantes/views.py
+++ django/sitio_web/restaurantes/views.py
@@ -8,3 +8,15 @@ def index(request):
 def test(request):
     context = {}   # Aquí van la las variables para la plantilla
     return render(request,'test.html', context)
+
+def buscar(request):
+    context = {
+            "resta": restaurants.objectcs[:5], # los cinco primeros
+         }
+         return render (requests, 'restaurantes/listar.html', context)
+
+def listar(request):
+    context = {
+            "resta": restaurants.objectcs[:5], # los cinco primeros
+         }
+         return render (requests, 'restaurantes/listar.html', context)
