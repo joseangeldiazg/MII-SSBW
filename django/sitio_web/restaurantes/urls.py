b34b3eda5c4f59e8cd8821de04d30dd5d5b1cffb
jadiazg02 <joseadiazg02@correo.ugr.es>
2017-05-31 17:35:37 +0200

    Añadidas plantillas. Avanza #10.

--- django/sitio_web/restaurantes/urls.py
+++ django/sitio_web/restaurantes/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
   url(r'^$', views.index, name='index'),
   url(r'^test/$', views.test, name='test'),
   url(r'^listar/$', views.listar, name='listar'),
-  url(r'^buscar/$', views.buscar, name='buscar'),
 ]
--- django/sitio_web/restaurantes/views.py
+++ django/sitio_web/restaurantes/views.py
@@ -9,14 +9,8 @@ def test(request):
     context = {}   # Aquí van la las variables para la plantilla
     return render(request,'test.html', context)
 
-def buscar(request):
-    context = {
-            "resta": restaurants.objectcs[:5], # los cinco primeros
-         }
-         return render (requests, 'restaurantes/listar.html', context)
-
 def listar(request):
     context = {
             "resta": restaurants.objectcs[:5], # los cinco primeros
          }
-         return render (requests, 'restaurantes/listar.html', context)
+    return render (requests, 'restaurantes/listar.html', context)
