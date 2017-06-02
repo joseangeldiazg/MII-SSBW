ed2e689792d8baf674140743f190b1e9ec9a5336
jadiazg02 <joseadiazg02@correo.ugr.es>
2017-06-02 13:36:15 +0200

    Finalizada tarea. Closes #10

--- django/sitio_web/restaurantes/urls.py
+++ django/sitio_web/restaurantes/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.index, name='index'),
-  url(r'^test/$', views.test, name='test'),
   url(r'^listar/$', views.listar, name='listar'),
+  url(r'^listar/introducir/$', views.introducir, name='introducir'),
+  url(r'^buscar/introducir/$', views.introducir, name='introducir'),
+  url(r'^buscar/$', views.buscar, name='buscar'),
 ]
--- django/sitio_web/restaurantes/views.py
+++ django/sitio_web/restaurantes/views.py
@@ -3,14 +3,23 @@ from .models import restaurants
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World!')
-
-def test(request):
-    context = {}   # Aquí van la las variables para la plantilla
-    return render(request,'test.html', context)
-
-def listar(request):
     context = {
             "resta": restaurants.objects[:5], # los cinco primeros
          }
+    return render (request, 'restaurantes/listar.html', context)
+
+def listar(request):
+    context = {
+            "resta": restaurants.objects[:2], # los cinco primeros
+         }
+    return render (request, 'restaurantes/listar.html', context)
+
+def introducir(request):
+    return render (request, 'restaurantes/introducir.html')
+
+def buscar(request):
+    parametro=request.GET.get('zona')
+    context={
+        "resta":restaurants.objects(borough=parametro)
+    }
     return render (request, 'restaurantes/listar.html', context)
