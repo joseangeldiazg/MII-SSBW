8b0d32d88652674e7d6aa5f52cf62de3358e96db
jadiazg02 <joseadiazg02@correo.ugr.es>
2017-06-28 17:01:43 +0200

    Funcionando página para cada restaurante básica. Avanza #12

--- django/sitio_web/restaurantes/urls.py
+++ django/sitio_web/restaurantes/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
   url(r'^listar/introducir/$', views.introducir, name='introducir'),
   url(r'^buscar/introducir/$', views.introducir, name='introducir'),
   url(r'^buscar/$', views.buscar, name='buscar'),
-  url(r'^restaurant/(?P<id>[0-9]+)$', views.restaurant, name='restaurant'),
+  url(r'^restaurant/(?P<id>[0-9]+)', views.restaurant, name='restaurant'),
 ]
