eec91bf02eabd31f852e9ec4f9814cf08f607945
jadiazg02 <joseadiazg02@correo.ugr.es>
2017-06-28 16:59:56 +0200

    Modificado script de añadir datos a la BD para añadir el id.

--- mongodb/populate.py
+++ mongodb/populate.py
@@ -3,6 +3,7 @@ from requests import *
 import datetime
 
 db=connect('test')
+db.drop_database('test')
 
 #Borramos la base de datos para que no se pisen los documentos
 
@@ -33,13 +34,13 @@ class restaurants(Document):
 
 #Vamos a bajar los restaurantes de la api de Google Maps
 
-def obtenerRestaurantes(bar, city, cuisine, borough):
+def obtenerRestaurantes(bar, city, cuisine, borough, id):
     url = 'http://maps.googleapis.com/maps/api/geocode/json?address='
     url = url+bar+city
     response = get(url)
     data = parseDireccion(response.json());
     dir = addr(building = data['number'] , street=data['street'], city=data['city'], zipcode=data['postal_code'], coord=[data['latitude'], data['longitude']])
-    r = restaurants(name=bar, cuisine=cuisine, borough=borough, address=dir)
+    r = restaurants(name=bar, restaurant_id=id, cuisine=cuisine, borough=borough, address=dir)
     r.save()
 #Funcion para parsear el restaurante en json
 
@@ -59,10 +60,15 @@ def parseDireccion(json):
         final['longitude'] = data.get("geometry", {}).get("location", {}).get("lng", None)
     return final
 
-obtenerRestaurantes("Casa Julio", "Granada", "Granaina", "Centro")
-obtenerRestaurantes("El Nido del Búho", "Granada", "Tapas", "Plaza de Toros")
-obtenerRestaurantes("Pizzametro", "Granada", "Italiana", "Centro")
-obtenerRestaurantes("La Bodeguica de Miguel del Rei", "Almería", "Tapas", "San Luís")
+obtenerRestaurantes("Casa Julio", "Granada", "Granaina", "Centro", 1)
+obtenerRestaurantes("El Nido del Búho", "Granada", "Tapas", "Plaza de Toros", 2)
+obtenerRestaurantes("Pizzametro", "Granada", "Italiana", "Centro", 3)
+obtenerRestaurantes("La Bodeguica de Miguel del Rei", "Almería", "Tapas", "San Luís", 4)
+
+obtenerRestaurantes("Casa Julio", "Granada", "Granaina", "Centro", 5)
+obtenerRestaurantes("El Nido del Búho", "Granada", "Tapas", "Plaza de Toros", 6)
+obtenerRestaurantes("Pizzametro", "Granada", "Italiana", "Centro", 7)
+obtenerRestaurantes("La Bodeguica de Miguel del Rei", "Almería", "Tapas", "San Luís", 8)
 
 
     #ne – not equal to
