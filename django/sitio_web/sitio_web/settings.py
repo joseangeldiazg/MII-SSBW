0a45e689898f2ca29cb4fb739ccfcfd390b0bbee
jadiazg02 <joseadiazg02@correo.ugr.es>
2017-07-03 19:47:38 +0200

    Estructura inicial de la API. avanza #19

--- django/sitio_web/sitio_web/settings.py
+++ django/sitio_web/sitio_web/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'registration',
+    'rest_framework',
+    'rest_framework_mongoengine',
     'restaurantes',
     'bootstrap4',
 ]
