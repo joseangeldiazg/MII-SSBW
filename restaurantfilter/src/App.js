feaf51c8dddcd25aa84df3331e2e0e825f66e986
jadiazg02 <joseadiazg02@correo.ugr.es>
2017-07-04 20:19:45 +0200

    App modificada y personalizada. closes #21

--- restaurantfilter/src/App.js
+++ restaurantfilter/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.ico';
 import './App.css';
 
 class App extends Component {
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Bienvenido a Restaurant Filter</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Esta app se conecta a la API desarrollada en <a href="https://github.com/joseangeldiazg/MII-SSBW/tree/master/django/sitio_web">este repositorio</a>
         </p>
       </div>
     );
